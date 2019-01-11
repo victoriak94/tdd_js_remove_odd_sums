4e6ca925935412d1ecb31b86c9466881da4dfa0c
victoriak94 <vkamila6@gmail.com>
2019-01-10 20:56:00 -0500

    Make function parameters an array and two key value pairs. Inside function, create new array that begins at index 1 of arr. Iterate over array. If a + b is an odd number, remove the key value pair at that index from array, starting at index 1. Return new array with even sums. Complete assignment

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function removeOddSum() {
+function removeOddSum(arr, a, b) {
+
+  evenSumArray = arr.slice(0);
+
+  for (i = 0; i < arr.length; i++) {
+    if ((arr[i][a] + arr[i][b]) % 2 !== 0) {
+      evenSumArray.splice(arr.indexOf(evenSumArray[i]), 1);
+    }
+  }
+  return evenSumArray;
 }
 
 module.exports = removeOddSum;
